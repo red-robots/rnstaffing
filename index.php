b6948cbc79115fce7889df21c355ec533602f14e
Fritz <fre.healy@gmail.com>
2017-04-11 17:47:12 -0400

    styling homepage major and minor

--- index.php
+++ index.php
@@ -16,11 +16,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-            <?php if ( have_posts() ) :?>
-
-                <?php get_template_part( 'template-parts/content', 'index' ); ?>
-
-            <?php endif;?>
+            <?php get_template_part( 'template-parts/content', 'index' ); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
--- template-parts/content-index.php
+++ template-parts/content-index.php
@@ -4,42 +4,116 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class("template-index"); ?>>
-    <div class="row-1">
-        <div class="row-1">
-            <?php get_template_part("template-parts/search","form"); ?>
-        </div><!--.row-1-->
-        <div class="row-2">
-
-        </div><!--.row-2-->
+<article id="post-<?php the_ID(); ?>" <?php post_class( "template-index" ); ?>>
+	<?php $post = get_post( 13 );
+	setup_postdata( $post );
+	$row_1_image = get_field( "row_1_image" ); ?>
+    <div class="row-1" <?php if ( $row_1_image ): ?>
+        style="background-image: url(<?php echo $row_1_image['url']; ?>);"
+	<?php endif; ?>>
+		<?php wp_reset_postdata(); ?>
+		<?php get_template_part( "template-parts/search", "form" ); ?>
     </div><!--.row-1-->
+	<?php $post = get_post( 13 );
+	setup_postdata( $post ); ?>
     <div class="row-2">
+		<?php $row_2_column_1_copy = get_field( "row_2_column_1_copy" );
+		$row_2_column_1_link       = get_field( "row_2_column_1_link" );
+		$row_2_column_2_copy       = get_field( "row_2_column_2_copy" );
+		$row_2_column_2_link       = get_field( "row_2_column_2_link" );
+		$row_2_column_3_copy       = get_field( "row_2_column_3_copy" );
+		$row_2_column_3_link       = get_field( "row_2_column_3_link" ); ?>
         <div class="col-1">
-
+			<?php if ( $row_2_column_1_link ): ?>
+            <a href="<?php echo $row_2_column_1_link; ?>">
+				<?php endif; ?>
+                <div class="wrapper">
+                    <div class="circle"><i class="fa fa-user-md"></i></div><!--.circle-->
+					<?php if ( $row_2_column_1_copy ): ?>
+                        <div class="copy"><?php echo $row_2_column_1_copy; ?></div><!--.copy-->
+					<?php endif; ?>
+                </div><!--.wrapper-->
+				<?php if ( $row_2_column_3_link ): ?>
+            </a>
+		<?php endif; ?>
         </div><!--.col-1-->
         <div class="col-2">
-
+			<?php if ( $row_2_column_2_link ): ?>
+            <a href="<?php echo $row_2_column_2_link; ?>">
+				<?php endif; ?>
+                <div class="wrapper">
+                    <div class="circle"><i class="fa fa-thumbs-o-up"></i></div><!--.circle-->
+					<?php if ( $row_2_column_2_copy ): ?>
+                        <div class="copy"><?php echo $row_2_column_2_copy; ?></div><!--.copy-->
+					<?php endif; ?>
+                </div><!--.wrapper-->
+				<?php if ( $row_2_column_3_link ): ?>
+            </a>
+		<?php endif; ?>
         </div><!--.col-2-->
         <div class="col-3">
-
+			<?php if ( $row_2_column_3_link ): ?>
+            <a href="<?php echo $row_2_column_3_link; ?>">
+				<?php endif; ?>
+                <div class="wrapper">
+                    <div class="circle"><i class="fa fa-heart-o"></i></div><!--.circle-->
+					<?php if ( $row_2_column_3_copy ): ?>
+                        <div class="copy"><?php echo $row_2_column_3_copy; ?></div><!--.copy-->
+					<?php endif; ?>
+                </div><!--.wrapper-->
+				<?php if ( $row_2_column_3_link ): ?>
+            </a>
+		<?php endif; ?>
         </div><!--.col-3-->
     </div><!--.row-2-->
     <div class="row-3">
-        <div class="col-1">
-
+		<?php $row_3_column_2_title = get_field( "row_3_column_2_title" );
+		$row_3_column_2_copy        = get_field( "row_3_column_2_copy" );
+		$row_3_column_1_image       = get_field( "row_3_column_1_image" );
+		$row_3_column_2_link        = get_field( "row_3_column_2_link" );
+		$row_3_column_2_link_text   = get_field( "row_3_column_2_link_text" ); ?>
+        <div class="col-1" <?php if ( $row_3_column_1_image ): ?>
+            style="background-image: url(<?php echo $row_3_column_1_image['url']; ?>);"
+		<?php endif; ?>>
+            <div class="wrapper"></div><!--.wrapper-->
         </div><!--.col-1-->
         <div class="col-2">
-
+            <div class="wrapper">
+                <div class="wrapper">
+					<?php if ( $row_3_column_2_title ): ?>
+                        <h2><?php echo $row_3_column_2_title; ?></h2>
+					<?php endif; ?>
+					<?php if ( $row_3_column_2_copy ): ?>
+                        <div class="copy">
+							<?php echo $row_3_column_2_copy; ?>
+                        </div><!--.copy-->
+					<?php endif; ?>
+                </div><!--.wrapper-->
+				<?php if ( $row_3_column_2_link && $row_3_column_2_link_text ): ?>
+                    <div class="link">
+                        <a href="<?php echo $row_3_column_2_link; ?>">
+							<?php echo $row_3_column_2_link_text; ?>
+                        </a>
+                    </div><!--link-->
+				<?php endif; ?>
+            </div><!--.wrapper-->
         </div><!--.col-2-->
-
     </div><!--.row-3-->
     <div class="row-4">
-        <div class="row-1">
-
-        </div><!--.row-1-->
-        <div class="row-2">
-
-        </div><!--.row-2-->
-
+		<?php $row_4_title = get_field( "row_4_title" );
+		$row_4_copy        = get_field( "row_4_copy" ); ?>
+		<?php if ( $row_4_title ): ?>
+            <h2><?php echo $row_4_title; ?></h2>
+            <div class="spacer"></div><!--.spacer-->
+		<?php endif; ?>
+		<?php if ( $row_4_copy ): ?>
+            <div class="copy">
+				<?php echo $row_4_copy; ?>
+            </div><!--.copy-->
+		<?php endif; ?>
+        <div class="email">
+            <!--insert email signup here-->
+        </div><!--.email-->
     </div><!--.row-4-->
+	<?php wp_reset_postdata(); ?>
 </article><!-- #post-## -->
