1f537c4f09d3176eb18f74f15b5a25e45d8bd73f
Fritz <fre.healy@gmail.com>
2017-04-12 15:36:59 -0400

    major styling complete homepage and header and footer, minor styling almost complete homepage

--- footer.php
+++ footer.php
@@ -14,9 +14,37 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="wrapper">
-
-        </div><!-- wrapper -->
+			<?php $facebook = get_field("facebook_link","option");
+			$instagram = get_field("instagram_link","option");
+			$twitter = get_field("twitter_link","option");
+			if($twitter||$instagram||$facebook):?>
+                <div class="row-1">
+					<?php if($facebook):?>
+                        <a href="<?php echo $facebook;?>" target="_blank">
+                            <i class="fa fa-facebook"></i>
+                        </a>
+					<?php endif;?>
+					<?php if($instagram):?>
+                        <a href="<?php echo $instagram;?>" target="_blank">
+                            <i class="fa fa-instagram"></i>
+                        </a>
+					<?php endif;?>
+					<?php if($twitter):?>
+                        <a href="<?php echo $twitter;?>" target="_blank">
+                            <i class="fa fa-twitter"></i>
+                        </a>
+					<?php endif;?>
+                </div><!--.row-1-->
+			<?php endif;?>
+            <div class="row-2">
+	            <?php wp_nav_menu( array( 'theme_location' => 'footer', 'menu_id' => 'footer-menu') ); ?>
+            </div><!--.row-2-->
+            <?php $copy = get_field("footer_copy","option");
+            if($copy):?>
+                <div class="row-3">
+                    <?php echo $copy;?>
+                </div><!--.row-3-->
+            <?php endif;?>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
